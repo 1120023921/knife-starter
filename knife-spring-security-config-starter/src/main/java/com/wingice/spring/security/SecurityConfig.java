f5760141da22e7be91278d8a9e3e9c952cbe3a0b
胡昊 <huhao9277@gmail.com>
2023-10-07 11:55:19 +0800

    1、spring security配置新写法

--- knife-spring-security-config-starter/src/main/java/com/wingice/spring/security/SecurityConfig.java
+++ knife-spring-security-config-starter/src/main/java/com/wingice/spring/security/SecurityConfig.java
@@ -5,9 +5,12 @@ import com.wingice.spring.security.handler.KnifeAuthenticationFailureHandler;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
@@ -22,7 +25,7 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 @AllArgsConstructor
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final LogoutHandler knifeLogoutHandler;
@@ -31,21 +34,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf()
-                .disable()
-                .cors()
-                .and()
-                .authorizeHttpRequests()
-                .anyRequest()
-                .permitAll()
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(knifeAuthenticationFailureHandler)
-                .accessDeniedHandler(knifeAccessDeniedHandler)
-                .and()
-                .logout().logoutSuccessUrl("/logout").addLogoutHandler(knifeLogoutHandler)
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .anyRequest().permitAll()
+                )
+                .exceptionHandling((exceptionHandling) ->
+                        exceptionHandling
+                                .authenticationEntryPoint(knifeAuthenticationFailureHandler)
+                                .accessDeniedHandler(knifeAccessDeniedHandler)
+                )
+                .logout((logout) ->
+                        logout.logoutSuccessUrl("/logout").addLogoutHandler(knifeLogoutHandler)
+                )
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
         // 构建过滤链并返回
         return http.build();
     }
