1bfee8be6edf6abd526dc720e446a172ca203b50
胡昊 <1120023921@qq.com>
2026-07-02 19:07:59 +0800

    springboot3 knife-log-starter修复

--- knife-log-starter/src/main/java/com/wingice/log/properties/KnifeLogProperties.java
+++ knife-log-starter/src/main/java/com/wingice/log/properties/KnifeLogProperties.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Collections;
+import java.util.List;
+
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "knife.log")
@@ -13,4 +16,9 @@ public class KnifeLogProperties {
     private String version = "1.0.0";
     //是否启用默认API
     private Boolean logApiEnable = false;
+    /**
+     * 可信代理 IP 列表
+     * 只有 remoteAddr 在此列表中的请求，才信任其代理头；否则忽略所有代理头，回退到 request.getRemoteAddr()
+     */
+    private List<String> trustedProxyIpList = Collections.emptyList();
 }
